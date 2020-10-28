99d091c17fffc35198ef10cc6921a22b168d22f0
Marijke Pante <marijke.pante@sfpd.fgov.be>
2020-10-28 11:30:20 +0100

    update test fizz

--- day1/01.demo.fizzbuzz/src/test/java/io/hackages/learning/fizzbuzz/TestFunctionnalityFizzBuzz.java
+++ day1/01.demo.fizzbuzz/src/test/java/io/hackages/learning/fizzbuzz/TestFunctionnalityFizzBuzz.java
@@ -1,15 +1,25 @@
 package io.hackages.learning.fizzbuzz;
-
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestFunctionnalityFizzBuzz {
 
+    public Main main = new Main();
+
     @Test
     public void test_fizzbuzz() {
-        // START HERE 
-        
+        // Goal : print a number
+        for (int i = 1 ; i< 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                assertEquals("FizzBuzz", main.transform(i), "FizzBuzz");
+            } else if (i % 3 == 0) {
+                assertEquals("Fizz", main.transform(i), "Fizz");
+            } else if (i % 5 == 0) {
+                assertEquals("Buzz", main.transform(i), "Buzz");
+            } else {
+                assertEquals(String.valueOf(i), main.transform(i), String.valueOf(i));
+            }
+        }
     }
 
 }
