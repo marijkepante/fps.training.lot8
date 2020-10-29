b27f071ae165ce85c2060484b0e1bb4536d91375
Tata DA SILVEIRA <dasilveira.tata@gmail.com>
2020-10-29 08:29:10 +0100

    update dependencies

--- day1/03.kata.bdd.gildedrose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ day1/03.kata.bdd.gildedrose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertEquals;
 public class GildedRoseTest {
 
     @Test
-    void foo() {
+    public void foo() {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
