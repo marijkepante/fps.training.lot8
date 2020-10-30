ca81d6270b3edb0b3af650445ac80c606fbec2ed
Tata DA SILVEIRA <dasilveira.tata@gmail.com>
2020-10-30 10:03:11 +0100

    scenario1

--- day2/04.testing.restapi/src/test/java/io/hackages/learning/exercise02/Exercise02.java
+++ day2/04.testing.restapi/src/test/java/io/hackages/learning/exercise02/Exercise02.java
@@ -4,6 +4,7 @@ import io.restassured.http.ContentType;
 import org.apache.http.HttpStatus;
 import org.junit.Test;
 
+import java.net.URI;
 import java.net.URISyntaxException;
 
 import static io.restassured.RestAssured.given;
@@ -30,6 +31,45 @@ public class Exercise02 {
                 .then()
                 .assertThat()
                 .statusCode(HttpStatus.SC_BAD_REQUEST);
+    }
+
+    @Test
+    public void test_scenario1() throws URISyntaxException {
+
+        // Fetch a record
+        URI uri = new URI("http://localhost:3000/catFacts/_id/591d9b2f227c1a0020d26823");
+        given().accept(ContentType.JSON)
+                .when()
+                .get(uri)
+                .then().assertThat().statusCode(HttpStatus.SC_OK);
+
+
+        // Delete the record
+        given().accept(ContentType.JSON)
+                .when()
+                .delete(uri)
+                .then().assertThat().statusCode(HttpStatus.SC_OK);
+
+        // Fetch the previous record
+        given().accept(ContentType.JSON)
+                .when()
+                .get(uri)
+                .then().assertThat().statusCode(HttpStatus.SC_NOT_FOUND);
+
+        /*
+        String jsonBody = "{\\r\\n\\t\\\"firstName\\\":\\\"John\\\",\\r\\n\\t\\\"lastName\\\":\\\"Wick\\\",\\r\\n\\t\\\"message\\\":\\\"I am coming for you !!!!\\\",\\r\\n\\t\\\"contact\\\": 123456789\\t\\r\\n}";
+
+        given()
+                .contentType(ContentType.JSON)
+                .and()
+                .body(jsonBody)
+                .when()
+                .post("https://myfakeapi.com/api/contactus")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_BAD_REQUEST);
+
+         */
     }
 
 }
